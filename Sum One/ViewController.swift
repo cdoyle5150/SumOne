a5ad132abed88fd9d18449568b3de732292c1504
CHAD DOYLE <chaddoyle@CHADs-MacBook-Pro.local>
2017-06-08 16:03:44 -0700

    Initial Commit

---
+++ Sum One/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sum One
+//
+//  Created by CHAD DOYLE on 6/8/17.
+//  Copyright © 2017 CHAD DOYLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
